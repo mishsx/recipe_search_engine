4b7b28057020a8311664749cb0b7406b2d846d42
Shubham_Mishra <smishra.on.ca@gmail.com>
2022-09-05 18:25:08 -0400

    Added Edmam API

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import requests
+
 # Create your views here.
 
+
+# 4cc0a10b
+# 5bdfee81719020965a984b95a247a86e	
+
+
 def index(request):
+    query="cheesecake"
+    response = requests.get("https://api.edamam.com/api/recipes/v2?type=public&q="+query+"&app_id=4cc0a10b&app_key=5bdfee81719020965a984b95a247a86e")
     return render(request, 'blog/index.html')
 
 def specific(request):
