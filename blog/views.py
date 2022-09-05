d913c338fd40d1569244e56a94ab7e4fab5d836a
Shubham_Mishra <smishra.on.ca@gmail.com>
2022-09-05 19:28:35 -0400

    Implemented Receipe Card layout on home screen; Added search bar UI;

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,9 @@ import requests
 def index(request):
     query="cheesecake"
     response = requests.get("https://api.edamam.com/api/recipes/v2?type=public&q="+query+"&app_id=4cc0a10b&app_key=5bdfee81719020965a984b95a247a86e")
-    return render(request, 'blog/index.html')
+    jsonResponse = response.json()
+    recipes = jsonResponse['hits']
+    return render(request, 'blog/index.html', {'recipes': recipes})
 
 def specific(request):
     return HttpResponse("This is the specific url")
